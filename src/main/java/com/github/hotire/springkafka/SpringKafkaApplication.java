576d941ef70a95f24a6ba4ea52c12672d0d530d9
user <gngh0101@gmail.com>
2020-01-07 23:07:43 +0900

    Move @EnableKafka into application.class

--- src/main/java/com/github/hotire/springkafka/SpringKafkaApplication.java
+++ src/main/java/com/github/hotire/springkafka/SpringKafkaApplication.java
@@ -2,7 +2,9 @@ package com.github.hotire.springkafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class SpringKafkaApplication {
 
