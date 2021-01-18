cf4d129bc134ffc2f29213ae97bd3c9bcac109fc
hotire <gngh0101@gmail.com>
2021-01-19 00:29:24 +0900

    Fix producerConfigs so that it's used kafkaProperties

--- src/main/java/com/github/hotire/springkafka/producer/SenderConfig.java
+++ src/main/java/com/github/hotire/springkafka/producer/SenderConfig.java
@@ -1,33 +1,27 @@
 package com.github.hotire.springkafka.producer;
 
-
 import java.util.Map;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 
+import lombok.RequiredArgsConstructor;
+
 @Configuration
+@RequiredArgsConstructor
 public class SenderConfig {
 
-  private final String bootstrapServers;
-
-  public SenderConfig(@Value("${kafka.bootstrap-servers}") String bootstrapServers) {
-    this.bootstrapServers = bootstrapServers;
-  }
+  private final KafkaProperties kafkaProperties;
 
   @Bean
   public Map<String, Object> producerConfigs() {
-    return Map.of(
-      ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-      ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
-      ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
-    );
+    return kafkaProperties.buildStreamsProperties();
   }
+
   @Bean
   public ProducerFactory<String, String> producerFactory() {
     return new DefaultKafkaProducerFactory<>(producerConfigs());
