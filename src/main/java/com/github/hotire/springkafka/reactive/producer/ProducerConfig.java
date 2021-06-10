2c3ead07077514cac14d4180813004159570dd1b
hotire <gngh0101@gmail.com>
2021-06-10 21:20:11 +0900

    Add ProducerConfig

---
+++ src/main/java/com/github/hotire/springkafka/reactive/producer/ProducerConfig.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springkafka.reactive.producer;
+
+public class ProducerConfig {
+//    public Flux<SendResult<String>> send() {
+//        return new ReactiveKafkaProducerTemplate<>()
+//    }
+}
