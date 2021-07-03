f75411159d4bf6adcf7506b74543256c840eb00b
hotire <gngh0101@gmail.com>
2021-07-04 00:08:27 +0900

    Implements isFull

--- src/main/java/com/github/hotire/springkafka/core/produce/ProducerBatchWrapper.java
+++ src/main/java/com/github/hotire/springkafka/core/produce/ProducerBatchWrapper.java
@@ -7,4 +7,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProducerBatchWrapper {
     private final ProducerBatch producerBatch;
+
+    public boolean isFull() {
+        return producerBatch.isFull();
+    }
 }
