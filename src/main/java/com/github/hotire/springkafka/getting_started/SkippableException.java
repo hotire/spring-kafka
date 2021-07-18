4ed0640d5fa6100daed4316b2fa409fbe67b7de4
hotire <gngh0101@gmail.com>
2021-07-18 17:05:09 +0900

    Use NestedRuntimeException instead of RuntimeException in SkippableException

--- src/main/java/com/github/hotire/springkafka/getting_started/SkippableException.java
+++ src/main/java/com/github/hotire/springkafka/getting_started/SkippableException.java
@@ -1,4 +1,13 @@
 package com.github.hotire.springkafka.getting_started;
 
-public class SkippableException extends RuntimeException{
+import org.springframework.core.NestedRuntimeException;
+
+public class SkippableException extends NestedRuntimeException {
+    public SkippableException(String msg) {
+        super(msg);
+    }
+
+    public SkippableException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
 }
