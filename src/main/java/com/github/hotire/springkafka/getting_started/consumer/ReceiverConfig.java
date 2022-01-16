57c566f419956f74336db71a8cd993d19cd6c9bd
hotire <gngh0101@gmail.com>
2022-01-16 20:37:48 +0900

    Fix ReceiverConfig

--- src/main/java/com/github/hotire/springkafka/getting_started/consumer/ReceiverConfig.java
+++ src/main/java/com/github/hotire/springkafka/getting_started/consumer/ReceiverConfig.java
@@ -1,10 +1,13 @@
 package com.github.hotire.springkafka.getting_started.consumer;
 
 import java.util.Map;
+import java.util.function.BiConsumer;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.KafkaException;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
@@ -13,10 +16,15 @@ import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.ContainerProperties.AckMode;
 import org.springframework.kafka.listener.SeekToCurrentErrorHandler;
+import org.springframework.kafka.listener.adapter.RetryingMessageListenerAdapter;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.retry.RecoveryCallback;
 import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.util.backoff.FixedBackOff;
+
+import com.github.hotire.springkafka.getting_started.SkippableException;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -46,7 +54,25 @@ public class ReceiverConfig {
         props.setAckMode(AckMode.MANUAL_IMMEDIATE);
         factory.setConsumerFactory(consumerFactory());
 //        factory.setErrorHandler((error, data) -> log.error("error : {}, data : {}", error.getMessage(), data, error));
-        factory.setErrorHandler(new SeekToCurrentErrorHandler(new FixedBackOff(0,  1)));
+        factory.setErrorHandler(new SeekToCurrentErrorHandler(new BiConsumer<ConsumerRecord<?, ?>, Exception>() {
+            @Override
+            public void accept(ConsumerRecord<?, ?> consumerRecord, Exception e) {
+                System.out.println(e);
+            }
+        }, new FixedBackOff(0, 10)));
+        factory.setRecoveryCallback((RecoveryCallback<Exception>) retryContext -> {
+            System.out.println();
+            KafkaException kafkaException = (KafkaException) retryContext.getLastThrowable();
+            if (kafkaException.contains(SkippableException.class)) {
+                retryContext.getAttribute(RetryingMessageListenerAdapter.CONTEXT_RECORD);
+                Object ack = retryContext.getAttribute(RetryingMessageListenerAdapter.CONTEXT_ACKNOWLEDGMENT);
+                if (ack instanceof Acknowledgment) {
+                    ((Acknowledgment) ack).acknowledge();
+                }
+                return kafkaException;
+            }
+            throw kafkaException;
+        });
         RetryTemplate retryTemplate = new RetryTemplate();
         ExponentialBackOffPolicy backOffPolicy = new ExponentialBackOffPolicy();
         backOffPolicy.setInitialInterval(1000L);
