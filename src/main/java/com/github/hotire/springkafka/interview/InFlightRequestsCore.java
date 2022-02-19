c980af7db03f1f667313c815211e9ddff857a1a6
hotire <gngh0101@gmail.com>
2022-02-19 15:54:45 +0900

    Learn send in InFlightRequests

--- src/main/java/com/github/hotire/springkafka/interview/InFlightRequestsCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/InFlightRequestsCore.java
@@ -1,7 +1,17 @@
 package com.github.hotire.springkafka.interview;
 
+import org.apache.kafka.clients.ClientRequest;
+import org.apache.kafka.common.requests.AbstractRequest;
+
 /**
  * @see org.apache.kafka.clients.InFlightRequests
  */
 public class InFlightRequestsCore {
+
+    /**
+     * @see org.apache.kafka.clients.NetworkClient#doSend(ClientRequest, boolean, long, AbstractRequest)
+     * @see org.apache.kafka.clients.InFlightRequests#add(InFlightRequest)
+     */
+    public void add(Object request) {
+    }
 }
