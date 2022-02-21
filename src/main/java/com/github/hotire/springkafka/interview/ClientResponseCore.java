5af78b8137da3ee640b14e1c718a6f2ffc967992
hotire <gngh0101@gmail.com>
2022-02-22 00:34:51 +0900

    Add onComplete

--- src/main/java/com/github/hotire/springkafka/interview/ClientResponseCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/ClientResponseCore.java
@@ -1,5 +1,7 @@
 package com.github.hotire.springkafka.interview;
 
+import java.util.List;
+
 import org.apache.kafka.clients.ClientResponse;
 
 /**
@@ -8,6 +10,7 @@ import org.apache.kafka.clients.ClientResponse;
 public class ClientResponseCore {
 
     /**
+     * @see org.apache.kafka.clients.NetworkClient#completeResponses(List)
      * @see ClientResponse#onComplete()
      */
     public void onComplete() {
