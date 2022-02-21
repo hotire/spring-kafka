9530e62d3d02bd5a5c11e3793694080e2efec127
hotire <gngh0101@gmail.com>
2022-02-22 00:33:50 +0900

    Learn ClientResponseCore

---
+++ src/main/java/com/github/hotire/springkafka/interview/ClientResponseCore.java
@@ -0,0 +1,16 @@
+package com.github.hotire.springkafka.interview;
+
+import org.apache.kafka.clients.ClientResponse;
+
+/**
+ * @see org.apache.kafka.clients.ClientResponse
+ */
+public class ClientResponseCore {
+
+    /**
+     * @see ClientResponse#onComplete()
+     */
+    public void onComplete() {
+    }
+
+}
