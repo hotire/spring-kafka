321796f3a6da39afef767e1886204b10781c9c23
hotire <gngh0101@gmail.com>
2022-03-12 00:22:48 +0900

    Add KafkaConsumerCore

---
+++ src/main/java/com/github/hotire/springkafka/interview/consumer/KafkaConsumerCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springkafka.interview.consumer;
+
+/**
+ * @see org.apache.kafka.clients.consumer.Consumer
+ * @see org.apache.kafka.clients.consumer.KafkaConsumer
+ */
+public class KafkaConsumerCore {
+}
