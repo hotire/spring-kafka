7cdad8534ec0216109834764a0fe51c4e0d34d5d
hotire <gngh0101@gmail.com>
2022-03-21 00:14:50 +0900

    Implement compose in RequestFuture

--- src/main/java/com/github/hotire/springkafka/interview/consumer/RequestFutureCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/consumer/RequestFutureCore.java
@@ -3,6 +3,7 @@ package com.github.hotire.springkafka.interview.consumer;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import org.apache.kafka.clients.consumer.internals.RequestFuture;
+import org.apache.kafka.clients.consumer.internals.RequestFutureAdapter;
 import org.apache.kafka.clients.consumer.internals.RequestFutureListener;
 
 /**
@@ -17,7 +18,27 @@ public class RequestFutureCore<T> extends RequestFuture<T> {
      */
     @Override
     public void addListener(RequestFutureListener<T> listener) {
+        super.addListener(listener);
+    }
 
+    /**
+     * @see RequestFuture#compose(RequestFutureAdapter)
+     */
+    @Override
+    public <S> RequestFuture<S> compose(final RequestFutureAdapter<T, S> adapter) {
+        final RequestFuture<S> adapted = new RequestFuture<>();
+        addListener(new RequestFutureListener<T>() {
+            @Override
+            public void onSuccess(T value) {
+                adapter.onSuccess(value, adapted);
+            }
+
+            @Override
+            public void onFailure(RuntimeException e) {
+                adapter.onFailure(e, adapted);
+            }
+        });
+        return adapted;
     }
 
     private void fireSuccess() {
