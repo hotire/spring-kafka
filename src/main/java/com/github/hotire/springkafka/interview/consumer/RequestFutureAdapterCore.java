d1af252ec71d65ba3499a444ee69245132f61dc0
hotire <gngh0101@gmail.com>
2022-03-23 00:13:47 +0900

    Implement RequestFutureAdapter

--- src/main/java/com/github/hotire/springkafka/interview/consumer/RequestFutureAdapterCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/consumer/RequestFutureAdapterCore.java
@@ -1,7 +1,15 @@
 package com.github.hotire.springkafka.interview.consumer;
 
+import org.apache.kafka.clients.consumer.internals.RequestFuture;
+import org.apache.kafka.clients.consumer.internals.RequestFutureAdapter;
+
 /**
  * @see org.apache.kafka.clients.consumer.internals.RequestFutureAdapter
  */
-public class RequestFutureAdapterCore {
+public class RequestFutureAdapterCore<F, T> extends RequestFutureAdapter<F, T> {
+
+    @Override
+    public void onSuccess(F value, RequestFuture<T> future) {
+        
+    }
 }
