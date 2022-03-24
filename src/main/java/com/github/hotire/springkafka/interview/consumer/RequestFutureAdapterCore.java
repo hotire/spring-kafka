3693e8282419f62fe75b26312647630acf884f7b
hotire <gngh0101@gmail.com>
2022-03-25 00:55:07 +0900

    Add RequestFutureAdapterCore

--- src/main/java/com/github/hotire/springkafka/interview/consumer/RequestFutureAdapterCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/consumer/RequestFutureAdapterCore.java
@@ -6,6 +6,20 @@ import org.apache.kafka.clients.consumer.internals.RequestFutureAdapter;
 /**
  * @see org.apache.kafka.clients.consumer.internals.RequestFutureAdapter
  * @see RequestFuture#compose(RequestFutureAdapter)
+ *
+ * - Rebalance
+ * @see org.apache.kafka.clients.consumer.internals.AbstractCoordinator.JoinGroupResponseHandler
+ * @see org.apache.kafka.clients.consumer.internals.AbstractCoordinator.SyncGroupResponseHandler
+ *
+ * - Offst Init
+ * @see org.apache.kafka.clients.consumer.internals.ConsumerCoordinator.OffsetFetchResponseHandler
+ *
+ * - Offset commit
+ * @see org.apache.kafka.clients.consumer.internals.ConsumerCoordinator.OffsetCommitResponseHandler
+ *
+ * - Heartbeat
+ * @see org.apache.kafka.clients.consumer.internals.ConsumerCoordinator.HeartbeatResponseHandler
+ *
  */
 public class RequestFutureAdapterCore<F, T> extends RequestFutureAdapter<F, T> {
 
