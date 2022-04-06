29274bf43faf8ac8927b5f96d6e4da9823155292
hotire <gngh0101@gmail.com>
2022-04-07 00:22:27 +0900

    Learn Authenticator, TransportLayer

--- src/main/java/com/github/hotire/springkafka/interview/client/KafkaChannelCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/client/KafkaChannelCore.java
@@ -2,6 +2,9 @@ package com.github.hotire.springkafka.interview.client;
 
 /**
  * @see org.apache.kafka.common.network.KafkaChannel
+ * @see org.apache.kafka.common.network.Authenticator  Kafka 클라이언트 인증 과정
+ * @see org.apache.kafka.common.network.TransportLayer SocketChannel과 SelectionKey를 사용한 전송 동작
  */
 public class KafkaChannelCore {
+
 }
