48233b5b3f472736b7feccdeea9d3b5fbb0dd218
hotire <gngh0101@gmail.com>
2022-04-07 00:07:27 +0900

    IdleExpiryManagerCore

--- src/main/java/com/github/hotire/springkafka/interview/client/IdleExpiryManagerCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/client/IdleExpiryManagerCore.java
@@ -1,8 +1,8 @@
 package com.github.hotire.springkafka.interview.client;
 
 /**
+ * @see org.apache.kafka.clients.CommonClientConfigs.CONNECTIONS_MAX_IDLE_MS_CONFIG
  * @see org.apache.kafka.common.network.Selector.IdleExpiryManager
- *
  *  this.idleExpiryManager = connectionMaxIdleMs < 0 ? null : new IdleExpiryManager(time, connectionMaxIdleMs);
  */
 public class IdleExpiryManagerCore {
