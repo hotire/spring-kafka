7146e9f73fdac56fa6633431edd65b5a695af612
hotire <gngh0101@gmail.com>
2022-04-10 22:47:35 +0900

    Learn MetadataUpdater

---
+++ src/main/java/com/github/hotire/springkafka/interview/client/MetadataUpdaterCore.java
@@ -0,0 +1,4 @@
+package com.github.hotire.springkafka.interview.client;
+
+public class MetadataUpdaterCore {
+}
---
+++ src/main/java/com/github/hotire/springkafka/interview/client/NodeCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springkafka.interview.client;
+
+/**
+ * @see org.apache.kafka.common.Node
+ */
+public class NodeCore {
+}
