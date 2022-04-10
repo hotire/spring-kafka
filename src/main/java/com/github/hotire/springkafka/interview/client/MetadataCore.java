fd912490afd8cd8a27273ee67ab3187e3e417d82
hotire <gngh0101@gmail.com>
2022-04-11 00:39:37 +0900

    Learn bootstrap in Metadata

--- src/main/java/com/github/hotire/springkafka/interview/client/MetadataCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/client/MetadataCore.java
@@ -1,7 +1,17 @@
 package com.github.hotire.springkafka.interview.client;
 
+import java.net.InetSocketAddress;
+import java.util.List;
+
 /**
  * @see org.apache.kafka.clients.Metadata
  */
 public class MetadataCore {
+
+    /**
+     * @see org.apache.kafka.clients.Metadata#bootstrap(List, long)
+     */
+    public synchronized void bootstrap(List<InetSocketAddress> addresses, long now) {
+
+    }
 }
