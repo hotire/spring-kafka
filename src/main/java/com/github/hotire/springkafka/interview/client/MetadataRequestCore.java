df16012cf6ed2881ef423b19a433c1e930306bd1
hotire <gngh0101@gmail.com>
2022-04-13 00:07:08 +0900

    Learn MetadataRequestCore

---
+++ src/main/java/com/github/hotire/springkafka/interview/client/MetadataRequestCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springkafka.interview.client;
+
+/**
+ * @see org.apache.kafka.common.requests.MetadataRequest
+ * @see org.apache.kafka.common.message.MetadataRequestData
+ */
+public class MetadataRequestCore {
+
+}
