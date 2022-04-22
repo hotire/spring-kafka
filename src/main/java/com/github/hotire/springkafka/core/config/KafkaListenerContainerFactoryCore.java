10ed8691ea6b336e3b1b4acc70010886f6375409
hotire <gngh0101@gmail.com>
2022-04-23 01:20:16 +0900

    Add KafkaListenerContainerFactoryCore

---
+++ src/main/java/com/github/hotire/springkafka/core/config/KafkaListenerContainerFactoryCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springkafka.core.config;
+
+/**
+ * @see org.springframework.kafka.config.KafkaListenerContainerFactory
+ * @see org.springframework.kafka.config.AbstractKafkaListenerContainerFactory
+ * @see org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+ */
+public class KafkaListenerContainerFactoryCore {
+}
