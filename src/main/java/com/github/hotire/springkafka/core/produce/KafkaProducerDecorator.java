108043394de02eb77d4851eee68690cdeda95fd3
hotire <gngh0101@gmail.com>
2022-04-23 01:20:59 +0900

    Update boot version 2.4.5

--- src/main/java/com/github/hotire/springkafka/core/produce/KafkaProducerDecorator.java
+++ src/main/java/com/github/hotire/springkafka/core/produce/KafkaProducerDecorator.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Future;
 
+import org.apache.kafka.clients.consumer.ConsumerGroupMetadata;
 import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.Producer;
@@ -38,6 +39,11 @@ public class KafkaProducerDecorator<K, V> implements Producer<K, V> {
     @Override
     public void sendOffsetsToTransaction(Map<TopicPartition, OffsetAndMetadata> offsets, String consumerGroupId) throws ProducerFencedException {
         getDelegate().sendOffsetsToTransaction(offsets, consumerGroupId);
+    }
+
+    @Override
+    public void sendOffsetsToTransaction(Map<TopicPartition, OffsetAndMetadata> map, ConsumerGroupMetadata consumerGroupMetadata) throws ProducerFencedException {
+        
     }
 
     @Override
--- src/main/java/com/github/hotire/springkafka/core/produce/RecordAccumulatorWrapper.java
+++ src/main/java/com/github/hotire/springkafka/core/produce/RecordAccumulatorWrapper.java
@@ -29,8 +29,10 @@ public class RecordAccumulatorWrapper {
                                      byte[] value,
                                      Header[] headers,
                                      Callback callback,
-                                     long maxTimeToBlock) throws InterruptedException {
-        return recordAccumulator.append(tp, timestamp, key, value, headers, callback, maxTimeToBlock);
+                                     long maxTimeToBlock,
+                                     boolean abortOnNewBatch,
+                                     long nowMs) throws InterruptedException {
+        return recordAccumulator.append(tp, timestamp, key, value, headers, callback, maxTimeToBlock, abortOnNewBatch, nowMs);
     }
 
     public Map<Integer, List<ProducerBatch>> drain(Cluster cluster, Set<Node> nodes, int maxSize, long now) {
@@ -39,7 +41,8 @@ public class RecordAccumulatorWrapper {
 
     /**
      *
-     * @see org.apache.kafka.clients.producer.internals.RecordAccumulator#append(TopicPartition, long, byte[], byte[], Header[], Callback, long)
+     * @see org.apache.kafka.clients.producer.internals.RecordAccumulator#append(TopicPartition, long, byte[], byte[], Header[], Callback, long, boolean, long)
+     * @see org.apache.kafka.clients.producer.internals.RecordAccumulator#tryAppend(long, byte[], byte[], Header[], Callback, Deque, long)
      *
      * Deque의 Last에서 RecordBatch 하나를 꺼내서 Record를 저장할 공간이 있는지 확인한다.
      * 여유 공간이 있으면 해당 RecordBatch에 Record를 추가하고, 여유 공간이 없으면 새로운 RecordBatch를 생성해서 Last쪽으로 저장한다.
@@ -47,14 +50,14 @@ public class RecordAccumulatorWrapper {
      *   if (future == null) last.closeForRecordAppends(); 을 하고 null을 리턴한다.
      */
     public RecordAppendResult tryAppend(long timestamp, byte[] key, byte[] value, Header[] headers,
-                                        Callback callback, Deque<ProducerBatch> deque) {
+                                        Callback callback, Deque<ProducerBatch> deque, long nowMs) {
         ProducerBatch last = deque.peekLast();
         if (last != null) {
             FutureRecordMetadata future = last.tryAppend(timestamp, key, value, headers, callback, time.milliseconds());
             if (future == null) {
                 last.closeForRecordAppends();
             } else {
-                return new RecordAppendResult(future, deque.size() > 1 || last.isFull(), false);
+                return new RecordAppendResult(future, deque.size() > 1 || last.isFull(), false, false);
             }
         }
         return null;
