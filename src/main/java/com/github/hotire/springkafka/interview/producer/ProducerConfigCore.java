394e7dd1c5869876648c3f47eef864e977df1fad
hotire <gngh0101@gmail.com>
2022-06-19 23:58:41 +0900

    Add ProducerConfigCore

---
+++ src/main/java/com/github/hotire/springkafka/interview/producer/ProducerConfigCore.java
@@ -0,0 +1,8 @@
+package com.github.hotire.springkafka.interview.producer;
+
+/**
+ * http://kafka.apache.org/documentation.html#producerconfigs
+ * @see org.apache.kafka.clients.producer.ProducerConfig
+ */
+public class ProducerConfigCore {
+}
