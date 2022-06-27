a000b865ab73086460c6d0e49a86936fa83739f4
hotire <gngh0101@gmail.com>
2022-06-27 17:17:56 +0900

    Add ProducerConfig

--- src/main/java/com/github/hotire/springkafka/interview/producer/ProducerConfigCore.java
+++ src/main/java/com/github/hotire/springkafka/interview/producer/ProducerConfigCore.java
@@ -1,8 +1,29 @@
 package com.github.hotire.springkafka.interview.producer;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+
 /**
  * http://kafka.apache.org/documentation.html#producerconfigs
- * @see org.apache.kafka.clients.producer.ProducerConfig
+ * @see ProducerConfig
  */
 public class ProducerConfigCore {
+
+    /**
+     * @see ProducerConfig#BATCH_SIZE_CONFIG
+     * DEFAULT 16384
+     */
+    public static final String BATCH_SIZE_CONFIG = "batch.size";
+
+    /**
+     * @see ProducerConfig#LINGER_MS_CONFIG
+     * DEFAULT 0
+     */
+    public static final String LINGER_MS_CONFIG = "linger.ms";
+
+    /**
+     * @see ProducerConfig#ACKS_CONFIG
+     * DEFAULT 1
+     */
+    public static final String ACKS_CONFIG = "acks";
+
 }
