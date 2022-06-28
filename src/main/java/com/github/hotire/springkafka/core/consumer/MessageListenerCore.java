804d1ef260dce532d74d61f47c79ee04ae6d6693
hotire <gngh0101@gmail.com>
2022-06-28 23:30:53 +0900

    Learn MessageListenerCore

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/MessageListenerCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springkafka.core.consumer;
+
+/**
+ * @see org.springframework.kafka.listener.MessageListener
+ * @see org.springframework.kafka.listener.AcknowledgingConsumerAwareMessageListener
+ * @see org.springframework.kafka.listener.adapter.RetryingMessageListenerAdapter
+ */
+public class MessageListenerCore {
+}
