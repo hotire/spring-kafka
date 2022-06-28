cad90c1e1133dd96c9d927755da4fb5a9525c9a2
hotire <gngh0101@gmail.com>
2022-06-28 23:39:05 +0900

    Add KafkaListenerEndpointRegistrarCore

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/KafkaListenerEndpointRegistrarCore.java
@@ -0,0 +1,17 @@
+package com.github.hotire.springkafka.core.consumer;
+
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerEndpoint;
+
+/**
+ * @see org.springframework.kafka.config.KafkaListenerEndpointRegistrar
+ */
+public class KafkaListenerEndpointRegistrarCore {
+
+    /**
+     * @see org.springframework.kafka.config.KafkaListenerEndpointRegistrar#registerEndpoint(KafkaListenerEndpoint, KafkaListenerContainerFactory)
+     */
+    public void registerEndpoint(KafkaListenerEndpoint endpoint, KafkaListenerContainerFactory<?> factory) {
+
+    }
+}
