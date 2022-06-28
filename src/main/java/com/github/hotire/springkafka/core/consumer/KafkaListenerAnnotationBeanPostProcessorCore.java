cc91eeebbc315b8fed78fc2c4e3e78deb1e8d362
hotire <gngh0101@gmail.com>
2022-06-29 00:04:32 +0900

    Add KafkaListenerAnnotationBeanPostProcessorCore

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/KafkaListenerAnnotationBeanPostProcessorCore.java
@@ -0,0 +1,7 @@
+package com.github.hotire.springkafka.core.consumer;
+
+/**
+ * @see org.springframework.kafka.annotation.KafkaListenerAnnotationBeanPostProcessor
+ */
+public class KafkaListenerAnnotationBeanPostProcessorCore {
+}
