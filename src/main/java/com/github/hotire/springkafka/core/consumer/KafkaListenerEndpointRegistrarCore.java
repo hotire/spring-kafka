6764fa300fb966f38600e350c32b1d272517cb8a
hotire <gngh0101@gmail.com>
2022-07-02 14:16:30 +0900

    Update KafkaListenerEndpointRegistrarCore

--- src/main/java/com/github/hotire/springkafka/core/consumer/KafkaListenerEndpointRegistrarCore.java
+++ src/main/java/com/github/hotire/springkafka/core/consumer/KafkaListenerEndpointRegistrarCore.java
@@ -1,17 +1,74 @@
 package com.github.hotire.springkafka.core.consumer;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerEndpoint;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistrar;
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
+import org.springframework.kafka.config.MultiMethodKafkaListenerEndpoint;
+import org.springframework.validation.Validator;
 
 /**
- * @see org.springframework.kafka.config.KafkaListenerEndpointRegistrar
+ * @see KafkaListenerEndpointRegistrar
  */
-public class KafkaListenerEndpointRegistrarCore {
+public class KafkaListenerEndpointRegistrarCore implements InitializingBean {
+
+    private KafkaListenerEndpointRegistry endpointRegistry;
+
+    private Validator validator;
+
+    private final List<KafkaListenerEndpointDescriptorCore> endpointDescriptors = new ArrayList<>();
 
     /**
-     * @see org.springframework.kafka.config.KafkaListenerEndpointRegistrar#registerEndpoint(KafkaListenerEndpoint, KafkaListenerContainerFactory)
+     * @see KafkaListenerEndpointRegistrar#registerEndpoint(KafkaListenerEndpoint, KafkaListenerContainerFactory)
      */
     public void registerEndpoint(KafkaListenerEndpoint endpoint, KafkaListenerContainerFactory<?> factory) {
+        KafkaListenerEndpointDescriptorCore descriptor = new KafkaListenerEndpointDescriptorCore(endpoint, factory);
+        endpointDescriptors.add(descriptor);
+    }
+
+    /**
+     * @see KafkaListenerEndpointRegistrar#registerAllEndpoints()
+     */
+    public void registerAllEndpoints() {
+        synchronized (this.endpointDescriptors) {
+            for (KafkaListenerEndpointDescriptorCore descriptor : this.endpointDescriptors) {
+                if (descriptor.endpoint instanceof MultiMethodKafkaListenerEndpoint
+                    && this.validator != null) {
+                    ((MultiMethodKafkaListenerEndpoint) descriptor.endpoint).setValidator(this.validator);
+                }
+                this.endpointRegistry.registerListenerContainer(
+                        descriptor.endpoint, resolveContainerFactory(descriptor));
+            }
+        }
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        registerAllEndpoints();
+    }
+
+    /**
+     * @see KafkaListenerEndpointRegistrar#resolveContainerFactory(org.springframework.kafka.config.KafkaListenerEndpointRegistrar.KafkaListenerEndpointDescriptor;)
+     */
+    private KafkaListenerContainerFactory<?> resolveContainerFactory(KafkaListenerEndpointDescriptorCore descriptor) {
+        return descriptor.containerFactory;
+    }
+
+    private static final class KafkaListenerEndpointDescriptorCore {
+
+        private final KafkaListenerEndpoint endpoint;
+
+        private final KafkaListenerContainerFactory<?> containerFactory;
+
+        private KafkaListenerEndpointDescriptorCore(KafkaListenerEndpoint endpoint,
+                                                    KafkaListenerContainerFactory<?> containerFactory) {
+            this.endpoint = endpoint;
+            this.containerFactory = containerFactory;
+        }
 
     }
 }
