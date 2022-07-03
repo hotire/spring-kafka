0467f701f263cac4d7ef8085cabe142b47eb6a39
hotire <gngh0101@gmail.com>
2022-07-03 23:22:25 +0900

    Update MessageListenerContainerCore

--- src/main/java/com/github/hotire/springkafka/core/consumer/MessageListenerContainerCore.java
+++ src/main/java/com/github/hotire/springkafka/core/consumer/MessageListenerContainerCore.java
@@ -2,6 +2,8 @@ package com.github.hotire.springkafka.core.consumer;
 
 /**
  * @see org.springframework.kafka.listener.MessageListenerContainer
+ * @see org.springframework.kafka.listener.AbstractMessageListenerContainer
+ * @see org.springframework.kafka.listener.ConcurrentMessageListenerContainer
  */
 public class MessageListenerContainerCore {
 }
