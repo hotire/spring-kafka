b1b5d02502f990def69507a935734bc51f06d89c
hotire <gngh0101@gmail.com>
2023-01-13 02:09:09 +0900

    Add KafkaAdminClientCore

---
+++ src/main/java/com/github/hotire/springkafka/core/admin/KafkaAdminClientCore.java
@@ -0,0 +1,13 @@
+package com.github.hotire.springkafka.core.admin;
+
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.KafkaAdminClient;
+
+
+/**
+ * @see AdminClient
+ * @see KafkaAdminClient
+ */
+public class KafkaAdminClientCore {
+
+}
