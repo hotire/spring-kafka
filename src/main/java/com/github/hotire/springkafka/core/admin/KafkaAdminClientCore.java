af26c9ba986b39ef446937718694c85a33576651
hotire <gngh0101@gmail.com>
2023-01-14 14:53:12 +0900

    Update KafkaAdminClientCore

--- src/main/java/com/github/hotire/springkafka/core/admin/KafkaAdminClientCore.java
+++ src/main/java/com/github/hotire/springkafka/core/admin/KafkaAdminClientCore.java
@@ -1,10 +1,12 @@
 package com.github.hotire.springkafka.core.admin;
 
+import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.KafkaAdminClient;
 
 
 /**
+ * @see Admin
  * @see AdminClient
  * @see KafkaAdminClient
  */
