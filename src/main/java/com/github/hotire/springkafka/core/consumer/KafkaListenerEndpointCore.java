7a2d31c7347c1e235290e5e9c166ddc0fa97dba2
hotire <gngh0101@gmail.com>
2023-01-25 01:19:10 +0900

    Add getId

--- src/main/java/com/github/hotire/springkafka/core/consumer/KafkaListenerEndpointCore.java
+++ src/main/java/com/github/hotire/springkafka/core/consumer/KafkaListenerEndpointCore.java
@@ -1,15 +1,21 @@
 package com.github.hotire.springkafka.core.consumer;
 
+import org.springframework.kafka.config.KafkaListenerEndpoint;
 import org.springframework.kafka.listener.MessageListenerContainer;
 import org.springframework.kafka.support.converter.MessageConverter;
 
 /**
- * @see org.springframework.kafka.config.KafkaListenerEndpoint
+ * @see KafkaListenerEndpoint
  * @see org.springframework.kafka.config.KafkaListenerEndpointAdapter
  * @see org.springframework.kafka.config.AbstractKafkaListenerEndpoint
  * @see org.springframework.kafka.config.MethodKafkaListenerEndpoint
  */
 interface KafkaListenerEndpointCore {
+
+    /**
+     * @see KafkaListenerEndpoint#getId()
+     */
+    String getId();
 
     /**
      * @see org.springframework.kafka.config.AbstractKafkaListenerEndpoint#setupListenerContainer(MessageListenerContainer, MessageConverter)
