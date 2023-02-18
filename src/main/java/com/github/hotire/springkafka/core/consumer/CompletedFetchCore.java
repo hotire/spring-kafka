0e643458926a35a89d15c20bef906a10991be9b9
hotire <gngh0101@gmail.com>
2023-02-18 12:46:51 +0900

    Add CompletedFetchCore

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/CompletedFetchCore.java
@@ -0,0 +1,11 @@
+package com.github.hotire.springkafka.core.consumer;
+
+import org.apache.kafka.clients.consumer.internals.Fetcher;
+
+/**
+ * @see Fetcher
+ * @see Fetcher.CompletedFetch
+ */
+public class CompletedFetchCore {
+
+}
