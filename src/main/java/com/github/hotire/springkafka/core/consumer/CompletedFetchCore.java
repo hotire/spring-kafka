daaf9f3f6b31102797bb0ee6b868919abb6ca4de
hotire <gngh0101@gmail.com>
2023-02-19 13:52:45 +0900

    Learn records in Fetcher.CompletedFetch

--- src/main/java/com/github/hotire/springkafka/core/consumer/CompletedFetchCore.java
+++ src/main/java/com/github/hotire/springkafka/core/consumer/CompletedFetchCore.java
@@ -1,6 +1,8 @@
 package com.github.hotire.springkafka.core.consumer;
 
 import org.apache.kafka.clients.consumer.internals.Fetcher;
+import org.apache.kafka.common.record.Record;
+import org.apache.kafka.common.utils.CloseableIterator;
 
 /**
  * @see Fetcher
@@ -18,12 +20,24 @@ public class CompletedFetchCore {
      */
     private boolean initialized = false;
 
+    /**
+     * @see Fetcher.CompletedFetch#records
+     */
+    private CloseableIterator<Record> records;
+
 
     /**
      * @see Fetcher.CompletedFetch#drain()
      */
     public void drain() {
 
+    }
+
+    /**
+     * @see Fetcher.CompletedFetch#nextFetchedRecord()
+     */
+    public Record nextFetchedRecord() {
+        return records.next();
     }
 
 }
