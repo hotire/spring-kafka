cd356e6ca2bb38c5fbaa964c31f1f2810ecea027
hotire <gngh0101@gmail.com>
2023-02-26 13:04:57 +0900

    Add ContainerPropertiesCore

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/ContainerPropertiesCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springkafka.core.consumer;
+
+import org.springframework.kafka.listener.ContainerProperties;
+
+/**
+ * @see ContainerProperties
+ */
+public class ContainerPropertiesCore {
+
+}
--- src/main/java/com/github/hotire/springkafka/core/spring/AsyncListenableTaskExecutorCore.java
+++ src/main/java/com/github/hotire/springkafka/core/spring/AsyncListenableTaskExecutorCore.java
@@ -2,11 +2,19 @@ package com.github.hotire.springkafka.core.spring;
 
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
 import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.core.task.AsyncListenableTaskExecutor;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
+
+/**
+ * @see AsyncListenableTaskExecutor
+ * @see SimpleAsyncTaskExecutor
+ */
 public interface AsyncListenableTaskExecutorCore {
 
     /**
-     * @see  KafkaMessageListenerContainer#doStart()
+     * @see KafkaMessageListenerContainer#doStart()
+     * @see AsyncListenableTaskExecutor#submitListenable(Runnable)
      */
     ListenableFuture<?> submitListenable(Runnable task);
 }
