eca3b63708b19efc3f6a445b625266a87273ae68
hotire <gngh0101@gmail.com>
2023-03-01 15:33:34 +0900

    Add SchedulingAwareRunnableCore

---
+++ src/main/java/com/github/hotire/springkafka/core/spring/SchedulingAwareRunnableCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springkafka.core.spring;
+
+import  org.springframework.scheduling.SchedulingAwareRunnable;
+
+/**
+ * @see SchedulingAwareRunnable
+ */
+public class SchedulingAwareRunnableCore {
+
+}
