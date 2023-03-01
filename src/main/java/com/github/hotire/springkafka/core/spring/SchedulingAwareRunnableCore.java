2f8b554be54d0f5be558924d4e778de3a9819e99
hotire <gngh0101@gmail.com>
2023-03-01 15:36:08 +0900

    ListenerConsumer

--- src/main/java/com/github/hotire/springkafka/core/spring/SchedulingAwareRunnableCore.java
+++ src/main/java/com/github/hotire/springkafka/core/spring/SchedulingAwareRunnableCore.java
@@ -1,10 +1,19 @@
 package com.github.hotire.springkafka.core.spring;
 
-import  org.springframework.scheduling.SchedulingAwareRunnable;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.scheduling.SchedulingAwareRunnable;
 
 /**
  * @see SchedulingAwareRunnable
+ * @see KafkaMessageListenerContainer.ListenerConsumer;
  */
 public class SchedulingAwareRunnableCore {
+
+    /**
+     * @see KafkaMessageListenerContainer.ListenerConsumer#run()
+     */
+    public void run() {
+
+    }
 
 }
