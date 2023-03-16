475ec579fa9dacd51747198c8fd495b223434037
hotire <gngh0101@gmail.com>
2023-03-17 03:09:26 +0900

    Update KafkaConsumerComponent

---
+++ src/main/java/com/github/hotire/springkafka/custom/KafkaConsumerComponent.java
@@ -0,0 +1,23 @@
+package com.github.hotire.springkafka.custom;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface KafkaConsumerComponent {
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+    String groupId();
+
+    String topic();
+
+    String containerFactory();
+}
--- src/main/java/com/github/hotire/springkafka/custom/KafkaListenerComponent.java
+++
@@ -1,5 +0,0 @@
-package com.github.hotire.springkafka.custom;
-
-public @interface KafkaListenerComponent {
-
-}
