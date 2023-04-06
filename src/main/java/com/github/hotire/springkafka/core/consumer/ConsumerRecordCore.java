a166fcb7b6a14c8701d3db86b0e22df8baedbade
hotire <gngh0101@gmail.com>
2023-04-07 03:11:39 +0900

    Add ConsumerRecordCore

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/ConsumerRecordCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springkafka.core.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+/**
+ * @see ConsumerRecord
+ */
+public class ConsumerRecordCore {
+
+}
