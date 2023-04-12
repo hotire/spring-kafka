0c179e4d781334e419239ab681f0f7e5f395aebf
hotire <gngh0101@gmail.com>
2023-04-13 00:47:52 +0900

    Add org.apache.kafka.clients.consumer.internals

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/ConsumerCoordinatorCore.java
@@ -0,0 +1,9 @@
+package com.github.hotire.springkafka.core.consumer;
+import org.apache.kafka.clients.consumer.internals.ConsumerCoordinator;
+
+/**
+ * @see ConsumerCoordinator
+ */
+public class ConsumerCoordinatorCore {
+
+}
