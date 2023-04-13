2aab164dab9f07c29de234b737bc8be59dbcbaa8
hotire <gngh0101@gmail.com>
2023-04-14 03:01:07 +0900

    Add CommitFailedExceptionCore

---
+++ src/main/java/com/github/hotire/springkafka/core/consumer/exception/CommitFailedExceptionCore.java
@@ -0,0 +1,10 @@
+package com.github.hotire.springkafka.core.consumer.exception;
+
+import org.apache.kafka.clients.consumer.CommitFailedException;
+
+/**
+ * @see CommitFailedException
+ */
+public class CommitFailedExceptionCore {
+
+}
