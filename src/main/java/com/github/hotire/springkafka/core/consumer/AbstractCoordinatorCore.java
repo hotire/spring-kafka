28d0551f9df202e0fd2974838d489362922214d1
hotire <gngh0101@gmail.com>
2023-04-15 12:56:24 +0900

    Add HeartbeatThread

--- src/main/java/com/github/hotire/springkafka/core/consumer/AbstractCoordinatorCore.java
+++ src/main/java/com/github/hotire/springkafka/core/consumer/AbstractCoordinatorCore.java
@@ -8,6 +8,18 @@ import org.apache.kafka.clients.consumer.internals.RequestFuture;
  */
 public class AbstractCoordinatorCore {
 
+    private class HeartbeatThread  {
+
+        public void run() {
+            final String leaveReason = "consumer poll timeout has expired. This means the time between subsequent calls to poll() " +
+                "was longer than the configured max.poll.interval.ms, which typically implies that " +
+                "the poll loop is spending too much time processing messages. " +
+                "You can address this either by increasing max.poll.interval.ms or by reducing " +
+                "the maximum size of batches returned in poll() with max.poll.records.";
+            maybeLeaveGroup(leaveReason);
+        }
+    }
+
     /**
      * @see AbstractCoordinator#maybeLeaveGroup(String)
      */
